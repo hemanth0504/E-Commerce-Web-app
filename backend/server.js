7a76905aec4cbff9adf4561728a35a6a90be819a
hemanth0504 <hemanth.teegala5@gmail.com>
2025-05-26 01:53:58 -0400

    setup server

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,26 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+import dbConnect from "./config/db.js"; 
+import authRouter from "./routes/auth.js"
 
 dotenv.config();
 
 const app = express();
+app.use(express.json());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true              
+}));
 
 
-const PORT = process.env.PORT || 3000;
+app.get("/",(req,res)=>{
+      res.send("hello world");
+})
+
+
+const PORT = process.env.PORT || 4000
+
+app.listen(PORT,()=>{
+      console.log("server is starting on 4000")
+})
