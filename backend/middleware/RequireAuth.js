e05f195d9cb2035cc72368f2411c70890edff6c6
hemanth0504 <hemanth.teegala5@gmail.com>
2025-05-26 01:55:08 -0400

    JWT Middleware

---
+++ backend/middleware/RequireAuth.js
@@ -0,0 +1,21 @@
+const requireAuth = (req,res,next)=>{
+
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return res.status(401).json({ msg: "Missing token" });
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = decoded; // { userId, email }
+      next();
+    } catch (err) {
+      return res.status(401).json({ msg: "Invalid or expired token" });
+    }
+  };
+
+
+  export default requireAuth;
